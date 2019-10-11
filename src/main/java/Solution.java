d77244f3f336fa3da65b03e5539987fa5e502dd2
Hancock <bai.szsy@qq.com>
2019-10-11 22:34:37 +0800

    [TECH] - Add solution of [报数]

--- src/main/java/Solution.java
+++ src/main/java/Solution.java
@@ -1,3 +1,5 @@
+import javafx.util.Pair;
+
 /**
  * Solution class is saving all the solution function for leetcode questions.
  */
@@ -35,4 +37,57 @@ public class Solution {
         }
         return -1;
     }
+
+
+    /**
+     * 报数序列是一个整数序列，按照其中的整数的顺序进行报数，得到下一个数。其前五项如下：
+     *
+     * 1.     1
+     * 2.     11
+     * 3.     21
+     * 4.     1211
+     * 5.     111221
+     *
+     * 1 被读作  "one 1"  ("一个一") , 即 11。
+     * 11 被读作 "two 1s" ("两个一"）, 即 21。
+     * 21 被读作 "one 2",  "one 1" （"一个二" ,  "一个一") , 即 1211。
+     *
+     * 给定一个正整数 n（1 ≤ n ≤ 30），输出报数序列的第 n 项。
+     *
+     * 注意：整数顺序将表示为一个字符串。
+     *
+     * @param n 正整数n
+     * @return 报数序列第n项
+     */
+    public String countAndSay(int n) {
+        String result = "1";
+        for (int index = 0; index < n-1; index++) {
+            result = say(result);
+        }
+        return result;
+    }
+
+    Pair<String, String> cutFirstSameDigit(String number) {
+        String currentDigit = number.substring(0, 1);
+        int index = 0;
+        do {
+            index ++;
+        } while (index+1<=number.length() && currentDigit.equals(number.substring(index, index+1)));
+        String result = index + currentDigit;
+        String leftString = number.substring(index);
+        return new Pair<>(result, leftString);
+    }
+
+
+    String say(String number) {
+        StringBuilder resultBuilder = new StringBuilder();
+        do {
+            Pair<String, String> resultAndLeftString = cutFirstSameDigit(number);
+            String result = resultAndLeftString.getKey();
+            resultBuilder.append(result);
+            number = resultAndLeftString.getValue();
+        } while (!number.equals(""));
+        return resultBuilder.toString();
+    }
+
 }
--- src/test/java/SolutionTest.java
+++ src/test/java/SolutionTest.java
@@ -1,3 +1,4 @@
+import javafx.util.Pair;
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
@@ -11,6 +12,43 @@ public class SolutionTest {
     @Before
     public void init() {
         solution = new Solution();
+    }
+
+
+    /**
+     * _____________________________________________________________________________________________
+     * below is tests for [报数]
+     */
+    @Test
+    public void shouldReturn111221_given5() {
+        String result = solution.countAndSay(5);
+        Assert.assertEquals("111221", result);
+    }
+
+    @Test
+    public void shouldReturn1211_given4() {
+        String result = solution.countAndSay(4);
+        Assert.assertEquals("1211", result);
+    }
+
+
+    @Test
+    public void shouldReturn11_given1() {
+        String result = solution.countAndSay(1);
+        Assert.assertEquals("11", result);
+    }
+
+    @Test
+    public void shouldCut111_whenCutFirstSameDigit_given11122() {
+        Pair<String, String> resultAndLeftString = solution.cutFirstSameDigit("11122");
+        Assert.assertEquals("31", resultAndLeftString.getKey());
+        Assert.assertEquals("22", resultAndLeftString.getValue());
+
+    }
+
+    @Test
+    public void shouldSay3122_whenSayDigit_given11122() {
+        Assert.assertEquals("3122", solution.say("11122"));
     }
 
     /**
@@ -56,4 +94,6 @@ public class SolutionTest {
         int result = solution.strStr(haystack, needle);
         Assert.assertEquals(0, result);
     }
+
+
 }
