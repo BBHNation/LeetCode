988d27fe9c0e2d2437635fef2587ac956f2a9683
Hancock <bai.szsy@qq.com>
2019-10-11 14:17:10 +0800

    [TECH] - Solution of strStr Function.

--- src/main/java/Solution.java
+++ src/main/java/Solution.java
@@ -9,6 +9,8 @@ public class Solution {
      * 给定一个 haystack 字符串和一个 needle 字符串，在 haystack 字符串中找出 needle 字符串出现的第一个位置 (从0开始)。如果不存在，则返回  -1。
      *
      * 这里使用的是new subString的hashCode相等性质判断，没有用到KMP算法，在一定程度上，此算法和KMP时间复杂度一致
+     * JDK6中subString时间复杂度是O(1)，底层维护同一个char[]对象，hash值的计算也是O(1)复杂度，所以目前整体算法在
+     * JDK6中时间复杂度是稳定O(n)，空间复杂度是O(1). 但是JDK7之后，为了防止内存泄露，subString方法不再是维护同一个char[]，导致此算法时间复杂度增加为O(n*n)
      *
      * @param haystack 字符串
      * @param needle 字符串
@@ -24,10 +26,10 @@ public class Solution {
 
         int lengthOfNeedle = needle.length();
         for (int indexOfPiece = 0;
-            indexOfPiece < haystack.length() - lengthOfNeedle+1;
+            indexOfPiece < haystack.length() - lengthOfNeedle + 1;
             indexOfPiece++) {
-            String pieceString = haystack.substring(indexOfPiece, indexOfPiece+lengthOfNeedle);
-            if (pieceString.hashCode() == needle.hashCode()) {
+            if (haystack.substring(indexOfPiece, indexOfPiece + lengthOfNeedle).hashCode()
+                == needle.hashCode()) {
                 return indexOfPiece;
             }
         }
